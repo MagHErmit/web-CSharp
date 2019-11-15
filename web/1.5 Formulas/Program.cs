0087fd83df0edbf67b44dc8fc49ebc073a44e7f0
Vladislav <vladkakurin007@gmail.com>
2019-11-16 09:07:44 +1000

    Add 1.5 Formulas and 1.6 Text

---
+++ web/1.5 Formulas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1._5_Formulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+            double x;
+            while (true)
+            {
+                try
+                {
+                    x = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод");
+                }
+            }
+            Console.WriteLine(
+                Math.Atan(x + Math.Sqrt(1 + x*x))
+                );
+            Console.WriteLine(
+                (1 / (4 * Math.Sqrt(3))) * Math.Log((Math.Sqrt(x*x + 2) - (x * Math.Sqrt(3))/((Math.Sqrt(x * x + 2) + (x * Math.Sqrt(3))))))
+                );
+            Console.WriteLine(
+                (1 / 2 * Math.Sqrt(2) * Math.Atan(x * Math.Sqrt(2) / Math.Sqrt(1 + x * x * x * x)))
+                );
+        }
+    }
+}
---
+++ web/1.6 Text/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._6_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string s = Console.ReadLine();
+
+            int cnt = 0;
+            string LastLetters = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    cnt++;
+                }
+            }
+            s = s.Replace(".", "")
+                 .Replace(",", "")
+                 .Replace("-", " ")
+                 .Replace("!", "")
+                 .Replace("?", "")
+                 .Replace(":", "")
+                 .Replace(";", "")
+                 .Replace("(", "")
+                 .Replace(")", "")
+                 .Replace("\"", "")
+                 .Replace("   "," ")
+                 .Replace("  ", " ");
+
+            List<string> sa = new List<string>(s.Split(new char[] { ' ' }));
+            for (int i = 0; i < sa.Count; i++)
+            {
+                if(sa[i].Length == 1)
+                {
+                    sa.RemoveAt(i);
+                }
+                LastLetters += sa[i][sa[i].Length - 1];
+            }
+
+            Console.WriteLine($"Количество слов: {sa.Count};");
+            Console.WriteLine($"Количество символов без пробелов: {cnt};");
+            Console.WriteLine($"Соотношение количество символов без пробелов к количеству слов: {(double)cnt / sa.Count};");
+            Console.WriteLine($"Слово из последних символов слов: {LastLetters};");
+        }
+    }
+}
