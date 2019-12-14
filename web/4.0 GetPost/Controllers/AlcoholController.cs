d7df5069ea45e6bb145cddf809d3afe35c767471
Владислав Какурин <vlad_kakurin@mail.ru>
2019-12-14 23:10:02 +1000

    Add 4.0 GetPost

---
+++ web/4.0 GetPost/Controllers/AlcoholController.cs
@@ -0,0 +1,28 @@
+﻿using _4._0_GetPost.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4._0_GetPost.Controllers
+{
+    public class AlcoholController : Controller
+    {
+        EssenceContext db;
+        public AlcoholController(EssenceContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Alcohols.ToList());
+        }
+        [HttpGet]
+        public IActionResult Properties(int? id)
+        {
+            //ViewBag.alc = db.Alcohols.Find(id);
+            return View();
+        }
+    }
+}
---
+++ web/4.0 GetPost/SampleData.cs
@@ -0,0 +1,35 @@
+﻿using _4._0_GetPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4._0_GetPost
+{
+    public class SampleData
+    {
+        public static void Initialize(EssenceContext context)
+        {
+            if (!context.Alcohols.Any())
+            {
+                context.Alcohols.AddRange(
+                    new Alcohol
+                    {
+                        Brand = "Jack Daniel's",
+                        Type = "Виски",
+                        Strength = 40, 
+                        Volume = 5
+                    },
+                    new Alcohol
+                    {
+                        Brand = "Триозерье",
+                        Type = "Водка",
+                        Strength = 40,
+                        Volume = 7
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
